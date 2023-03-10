e75b2691bb808ebb0d9b2b6370d5e288e65cb029
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-03-10 18:57:35 -0300

    Ex 2 - [Cadastrar filme] Criação de função de cadastro e validação

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ function cadastrar() {
     film.title = document.getElementById("title").value;
     film.rating = document.getElementById("rating").value;
     film.length = document.getElementById("length").value;
-  
+    //Favorito e asistido (em ingles igual ao resto do meu codigo)
+    film.favourite = false;
+    film.wathced = false;
   
   let titles = [];
   films.forEach(function(existingFilm) {
     titles.push(existingFilm.title);
   });
   if (titles.includes(film.title)) {
-    alert(`filme ${film.title} já está cadastrado!`);
+    alert(`Já possui um filme com esse mesmo título`);
     return;
   }
-  
+
   films.push(film);
-  alert(`filme ${film.title} foi cadastrado com suceso!`);
+  alert(`filme ${film.title} adicionado com suceso!`);
   localStorage.setItem("films", JSON.stringify(films));
   
   document.getElementById("films-form").reset(); 
