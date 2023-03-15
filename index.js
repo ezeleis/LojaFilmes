e22535e8672ed164beb97085e73f19a20c21ffee
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-03-15 19:19:13 -0300

    test: posters fetch from TMDB

--- index.js
+++ index.js
@@ -40,11 +40,13 @@ function listFilms (){
  films.forEach(createCard);
   };
 
-function createCard(film){
-let card = document.createElement("div");
+  let card;
+async function createCard(film){
+card = document.createElement("div");
 card.classList = "filmCard";
 let title = document.createElement("h1");
 title.innerHTML=film.title;
+title.id="filmTitle";
 let rating = document.createElement("img");
   if(film.rating==5){
     rating.src= "img/5-stars.jpg";
@@ -61,59 +63,40 @@ let rating = document.createElement("img");
   else if (film.rating==1){
     rating.src="img/1-star.jpg";
   }
-let poster = document.createElement("img");
+await getPoster(title.innerHTML);
+// poster.src = url;
 card.appendChild(title);
 card.appendChild(rating);
-card.appendChild(poster);
 document.getElementById("filmsList").appendChild(card);
 return card;
 }
 
 
 
-//console.log($.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb"));
+async function getPoster(title){
+  console.log(title);
+  await fetch("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query="+title)
+  .then((response) => {
+    r = response; 
+    console.log( 1, r);
+    r = r.json();
+    console.log(2, r);
+    return r;
+  } )
+  .then((data) => {
+    console.log( 3, data);
+    r= data.results;
+    console.log(4, r);
+    r = r[0];
+    console.log(5,r);
+    r= r.poster_path;
+    console.log(6,r);
+    let poster = document.createElement("img");
+    poster.id="poster";
+    poster.src= "http://image.tmdb.org/t/p/w500/"+ r;
+    obj = card.appendChild(poster);
+    console.log(obj);
+   })
 
 
-// $('#term').focus(function(){
-//   var full = $("#poster").has("img").length ? true : false;
-//   if(full == false){
-//      $('#poster').empty();
-//   }
-// });
-
-// var getPoster = function(){
-
-//     var film = $('#title').val();
-
-//      if(film == ''){
-
-//         $('#poster').html('<div class="alert"><strong>Oops!</strong> Try adding something into the search field.</div>');
-
-//      } else {
-
-//         $('#poster').html('<div class="alert"><strong>Loading...</strong></div>');
-
-//         $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function(json) {
-//            if (json != "Nothing found."){                 
-// console.log(json);
-//                  $('#poster').html('<p>Your search found: <strong>' + json.results[0].title + '</strong></p><img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >');
-//               } else {
-//                  $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=goonies&callback=?", function(json) {
-                   
-//                    console.log(json);
-//                     $('#poster').html('<div class="alert"><p>We\'re afraid nothing was found for that search.</p></div><p>Perhaps you were looking for The Goonies?</p><img id="thePoster" src="http://image.tmdb.org/t/p/w500/' + json[0].poster_path + ' class="img-responsive" />');
-//                  });
-//               }
-//          });
-
-//       }
-
-//     return false;
-// }
-
-// $('#search').click(getPoster);
-// $('#term').keyup(function(event){
-//    if(event.keyCode == 13){
-//        getPoster();
-//    }
-// });
+}
