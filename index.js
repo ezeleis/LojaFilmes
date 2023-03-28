f9a474782c1041ecff6a8057102f5bda25821d32
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-03-27 22:28:40 -0300

    fix(watched time):fixed decimals feature(watched):watched span

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ favourites = films.filter((film)=>film.favourite);
 let titles = [];
 films.forEach((film)=>titles.push(film.title));
 
+let watchedTime=0;
+
 class Film{
   title
   rating
@@ -130,18 +132,21 @@ filmsList.appendChild(filmCard);
   resultsObjects.forEach((film)=>listar(film));
   
 }
-// function setFavourite(film) {
-//   film.favourite = !film.favourite;
-//   favourites = films.filter((film) => film.favourite);
-//   const favouriteIcon = document.getElementById(`${film.title} favourite`);
-//   favouriteIcon.src = film.favourite ? "img/heart-full.png" : "img/heart-empty.png";
-// }
+
 //Assistido e favoritos funcoes do ex 6
 function setWatched(film) {
   film.watched = !film.watched;
   favourites = films.filter((film) => film.watched);
+  
   const watchedIcon = document.getElementById(`${film.title} watched`);
   watchedIcon.src = film.watched ? "img/watched-yes.png" : "img/watched-no.png";
+
+  let filmHours = film.length/60; 
+
+  film.watched?(watchedTime+=filmHours):(watchedTime-=filmHours);
+  
+  watchedTime?document.getElementById('watched-hours').innerHTML = watchedTime.toFixed(2) +" horas asistidas":document.getElementById('watched-hours').innerHTML="";
+
 }
 
 function setFavourite(film){
